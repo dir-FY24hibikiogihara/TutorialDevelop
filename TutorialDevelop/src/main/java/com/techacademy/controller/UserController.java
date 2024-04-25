6a224b6739cf936972d50b51ed077cdfb709b768
Hibikiengawa <hibiki.ogihara.x996@dirbato.co.jp>
2024-04-25 14:17:53 +0900

    21４回目のコミット

--- TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 
@@ -24,4 +28,22 @@ public class UserController {
         return "user/list";
     }
 
-}
+    
+    /**User登録画面を表示*/
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        //User登録画面に遷移
+        return "user/register";
+    }
+
+    
+    /**User登録処理*/
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        //User登録
+        service.saveUser(user);
+        //一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    
+    }
